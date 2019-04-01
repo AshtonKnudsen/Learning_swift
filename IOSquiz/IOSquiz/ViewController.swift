78b021b6d3bbba7c260127a17b131854f368c1a0
ashton <Ashtonknudsen@gmail.com>
2019-03-31 21:52:21 -0700

    Create ViewController.swift

---
+++ IOSquiz/IOSquiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSquiz
+//
+//  Created by Ashton Knudsen on 3/31/19.
+//  Copyright © 2019 Ashton Knudsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
