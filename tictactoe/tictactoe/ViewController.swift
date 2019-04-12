db36d7e11047035615def64ff5bcac5d4d5d3d15
ashton <Ashtonknudsen@gmail.com>
2019-04-11 20:59:11 -0700

    Create ViewController.swift

---
+++ tictactoe/tictactoe/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  tictactoe
+//
+//  Created by Ashton Knudsen on 4/11/19.
+//  Copyright © 2019 Ashton Knudsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
